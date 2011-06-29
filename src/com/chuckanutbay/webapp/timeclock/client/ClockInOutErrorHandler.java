b3812f1859e042cc4c1c5461c184d202239bf1a7
Mitchell Loeppky <mitchell@loeppky.com>
2011-06-29 09:29:23 -0700

    Added comments to the Interfaces

--- src/com/chuckanutbay/webapp/timeclock/client/ClockInOutErrorHandler.java
+++ src/com/chuckanutbay/webapp/timeclock/client/ClockInOutErrorHandler.java
@@ -4,8 +4,16 @@ import com.chuckanutbay.webapp.common.shared.BarcodeDto;
 import com.chuckanutbay.webapp.common.shared.EmployeeDto;
 
 public interface ClockInOutErrorHandler {
-
+	
+	/**
+	 * Handles any mistakes by a user when clocking in. 
+	 * @param barcode The {@link BarcodeDto} associated with the error.
+	 */
 	public void onClockInError(BarcodeDto barcode);
 	
+	/**
+	 * Handles any mistakes by a user when clocking out. 
+	 * @param employee The {@link EmployeeDto} associated with the error.
+	 */
 	public void onClockOutError(EmployeeDto employee);
 }
--- src/com/chuckanutbay/webapp/timeclock/client/ScanInOutHandler.java
+++ src/com/chuckanutbay/webapp/timeclock/client/ScanInOutHandler.java
@@ -9,9 +9,21 @@ import com.chuckanutbay.webapp.common.shared.BarcodeDto;
  */
 public interface ScanInOutHandler {
 	
+	/**
+	 * Handles a scan event.
+	 * @param barcode The {@link BarcodeDto} of the scan.
+	 */
 	public void onScan(BarcodeDto barcode);
 	
+	/**
+	 * Handles a clock in scan event.
+	 * @param barcode The {@link BarcodeDto} of the scan in.
+	 */
 	public void onClockInScan(BarcodeDto barcode);
 	
+	/**
+	 * Handles a clock out scan event.
+	 * @param barcode The {@link BarcodeDto} of the scan out.
+	 */
 	public void onClockOutScan(BarcodeDto barcode);
 }
