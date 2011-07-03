73618b8d2a9f8fafec1c5f2ebca49dde967d4aa5
Steve Loeppky <stvn@loeppky.com>
2011-07-02 22:47:52 -0700

    Fixed business object unit tests by configuring them to use H2 instead of HSQLDB.  I also wrote an example unit test for InventoryLotHibernateDao for Mitch to be able to follow.

--- src/com/chuckanutbay/businessobjects/InventoryItem.java
+++ src/com/chuckanutbay/businessobjects/InventoryItem.java
@@ -2,16 +2,19 @@ package com.chuckanutbay.businessobjects;
 
 // Generated Oct 20, 2010 1:24:33 PM by Hibernate Tools 3.4.0.Beta1
 
+import static javax.persistence.GenerationType.IDENTITY;
+
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
 import com.google.common.base.Objects;
 
 /**
@@ -41,7 +44,7 @@ public class InventoryItem implements java.io.Serializable {
 	}
 
 	@Id
-	@Basic
+	@GeneratedValue(strategy = IDENTITY)
 	@Column(name = "id", unique = true, nullable = false, length = 10)
 	public String getId() {
 		return this.id;
