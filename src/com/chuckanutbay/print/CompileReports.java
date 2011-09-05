0727760067f22a09ee4328ae090675df01ff2aa4
Steve Loeppky <stvn@loeppky.com>
2011-09-05 16:45:48 -0700

    Updated to find report files in resources/reports.

--- src/com/chuckanutbay/print/CompileReports.java
+++ src/com/chuckanutbay/print/CompileReports.java
@@ -1,16 +1,33 @@
 package com.chuckanutbay.print;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperReport;
+
+import com.google.common.io.Closeables;
 
 public class CompileReports {
 	
 	public static void main(String[] args) {
 		try {
 			for (String reportName : ReportUtil.getReportStrings()) {
-				JasperCompileManager.compileReport("war/reports/" + reportName + ".jrxml");
+				JasperReport report = JasperCompileManager.compileReport("resources/reports/" + reportName + ".jrxml");
+				// TODO: log where writing the file.
+				ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(new File("resources/reports/" + reportName + ".jasper")));
+				outputStream.writeObject(report);
+				Closeables.closeQuietly(outputStream);
 			}
 		} catch (JRException e) {
+			throw new RuntimeException(e);
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
