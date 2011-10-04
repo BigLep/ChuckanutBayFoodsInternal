049f69bb2fa88ddb41af93a45457a39323800d6e
Mitchell Loeppky <mitchell@loeppky.com>
2011-10-04 16:15:02 -0700

    Fixed a bug in overtime calculation

--- src/com/chuckanutbay/webapp/common/shared/WeekReportData.java
+++ src/com/chuckanutbay/webapp/common/shared/WeekReportData.java
@@ -41,7 +41,7 @@ public class WeekReportData implements Serializable {
 	public void addHours(double hours) {
 		hoursNormalPay += hours;
 		if ((hoursNormalPay + prePayPeriodHours) > 40) {
-			hoursOvertime = (hoursNormalPay + prePayPeriodHours) - 40;
+			hoursOvertime = (hoursNormalPay + prePayPeriodHours + hoursOvertime) - 40;
 			hoursNormalPay = (40 - prePayPeriodHours);
 		}
 	}
