d42a47393fb7b35ff01b90d17a2ba176b8b24e29
Mitchell Loeppky <mitchell@loeppky.com>
2012-03-27 13:04:59 -0700

    Updated the Dashboard style a little bit Setup the Dashboard to use my Composed Widgets

--- src/com/chuckanutbay/webapp/dashboard/client/Dashboard.java
+++ src/com/chuckanutbay/webapp/dashboard/client/Dashboard.java
@@ -1,24 +1,25 @@
 package com.chuckanutbay.webapp.dashboard.client;
 
 import static com.chuckanutbay.reportgeneration.Print.HP_WIRELESS_P1102W;
-import static com.chuckanutbay.webapp.common.client.GwtWidgetHelper.H_ALIGN_CENTER;
-import static com.chuckanutbay.webapp.common.client.GwtWidgetHelper.newLabel;
 import static com.chuckanutbay.webapp.common.client.ServiceUtils.createReportService;
 import static com.chuckanutbay.webapp.common.shared.ReportDto.DIGITAL_LABELS;
 import static com.chuckanutbay.webapp.common.shared.ReportDto.DIGITAL_LABEL_SUBREPORT;
 import static com.chuckanutbay.webapp.common.shared.ReportDto.SCHEDULE;
 import static com.chuckanutbay.webapp.dashboard.client.RpcHelper.createPrintReportCallback;
 import static com.google.common.collect.Lists.newArrayList;
+import static com.google.gwt.user.client.ui.HasHorizontalAlignment.ALIGN_CENTER;
+import gwthelper.client.ComposedButton;
+import gwthelper.client.ComposedLabel;
+import gwthelper.client.ComposedListBox;
+import gwthelper.client.ComposedRootPanel;
+import gwthelper.client.ComposedVPanel;
 
-import com.chuckanutbay.webapp.common.client.CbVerticalPanel;
 import com.chuckanutbay.webapp.common.shared.ReportDto;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 
 /**
@@ -26,62 +27,79 @@ import com.google.gwt.user.client.ui.TextBox;
  */
 public class Dashboard implements EntryPoint, ClickHandler, ReportPrinter {
 
-	private CbVerticalPanel mainPanel;
-	private final Button lotCodeManagerButton = new Button("Lot Code Manager");
-	private final Button timeClockButton = new Button("Time Clock");
-	private final Button timeClockReportButton = new Button("Time Clock Report");
-	private final Button trayLabelGeneratorButton = new Button("Tray Label Generator");
-	private final Button packagingTransactionManagerButton = new Button("Packaging Transaction Manager");
-	private final Button printLabelInventoryReportButton = new Button("Print Label Inventory Report");
-	private final Button printScheduleButton = new Button("Print Schedule Report");
-	private final Button printTestButton = new Button("Print Test");
+	private ComposedVPanel mainPanel;
+	private ComposedVPanel reportsSubPanel;
+	private final ComposedButton lotCodeManagerButton = new ComposedButton().setText("Lot Code Manager");
+	private final ComposedButton timeClockButton = new ComposedButton().setText("Time Clock");
+	private final ComposedButton timeClockReportButton = new ComposedButton().setText("Time Clock Report");
+	private final ComposedButton trayLabelGeneratorButton = new ComposedButton().setText("Tray Label Generator");
+	private final ComposedButton packagingTransactionManagerButton = new ComposedButton().setText("Packaging Transaction Manager");
+	private final ComposedButton labelInventoryReportButton = new ComposedButton().setText("Label Inventory");
+	private final ComposedButton scheduleButton = new ComposedButton().setText("Schedule");
+	private final ComposedButton testButton = new ComposedButton().setText("Test");
 	
-	private final ListBox printerListBox = new ListBox();
+	private final ComposedListBox<String> printerListBox = new ComposedListBox<String>();
 
 
 	@Override
 	public void onModuleLoad() {
-		mainPanel = new CbVerticalPanel()
-			.addWidget(newLabel("Chuckanut Bay Internal Web Apps", "titleLabel"), H_ALIGN_CENTER)
-			.setCellSpacing(10)
-			.setStyle("mainPanel");
+		mainPanel = new ComposedVPanel()
+			.addWithAlignment(ALIGN_CENTER, new ComposedLabel().setText("Chuckanut Bay Internal Web Apps").setStyleName("titleLabel"))
+			.setSpacing(10)
+			.setStyleName("mainPanel");
 		
-		for (Button button : newArrayList(lotCodeManagerButton, timeClockButton, timeClockReportButton, trayLabelGeneratorButton, packagingTransactionManagerButton, printLabelInventoryReportButton, printScheduleButton, printTestButton)) {
-			button.setStyleName("dashboardButton");
-			button.addClickHandler(this);
-			mainPanel.addWidget(button, H_ALIGN_CENTER);
+		for (ComposedButton button : newArrayList(lotCodeManagerButton, timeClockButton, timeClockReportButton, trayLabelGeneratorButton, packagingTransactionManagerButton)) {
+			button
+				.setStyleName("dashboardButton")
+				.addHandler(this);
+			mainPanel
+				.addWithAlignment(ALIGN_CENTER, button);
+		}
+		
+		reportsSubPanel = new ComposedVPanel()
+			.addWithAlignment(ALIGN_CENTER, new ComposedLabel().setText("Print Report:").setStyleName("titleLabel"))
+			.setSpacing(10)
+			.setStyleName("subPanel");
+		mainPanel.addWithAlignment(ALIGN_CENTER, reportsSubPanel);
+		
+		for (ComposedButton button : newArrayList(labelInventoryReportButton, scheduleButton, testButton)) {
+			button
+				.setStyleName("dashboardButton")
+				.addHandler(this);
+			reportsSubPanel
+				.addWithAlignment(ALIGN_CENTER, button);
 		}
 		
 		//***********************//
 		//** Add Printers here **//
 		//***********************//
-		printerListBox.addItem(HP_WIRELESS_P1102W);
+		printerListBox.addItem(HP_WIRELESS_P1102W, HP_WIRELESS_P1102W);
 		
-		mainPanel.addWidget(printerListBox, H_ALIGN_CENTER);
+		reportsSubPanel.addWithAlignment(ALIGN_CENTER, printerListBox);
 		
 		//Add mainPanel to rootPanel
-		RootPanel.get("Dashboard").add(mainPanel);
+		new ComposedRootPanel("Dashboard").add(mainPanel);
 	}
 
 
 	@Override
 	public void onClick(ClickEvent event) {
 		Object sender = event.getSource();
-		if (sender == lotCodeManagerButton) {
+		if (lotCodeManagerButton.equals(sender)) {
 			Window.open("LotCodeManager.html", "LotCodeManager", "");
-		} else if (sender == timeClockButton) {
+		} else if (timeClockButton.equals(sender)) {
 			Window.open("TimeClock.html", "TimeClock", "");
-		} else if (sender == timeClockReportButton) {
+		} else if (timeClockReportButton.equals(sender)) {
 			Window.open("TimeClockReport.html", "TimeClockReport", "");
-		} else if (sender == trayLabelGeneratorButton) {
+		} else if (timeClockReportButton.equals(sender)) {
 			Window.open("TrayLabelGenerator.html", "TrayLabelGenerator", "");
-		} else if (sender == packagingTransactionManagerButton) {
+		} else if (packagingTransactionManagerButton.equals(sender)) {
 			Window.open("PackagingTransactionManager.html", "PackagingTransactionManager", "");
-		} else if (sender == printLabelInventoryReportButton) {
+		} else if (labelInventoryReportButton.equals(sender)) {
 			print(new ReportDto().setName(DIGITAL_LABELS).setSubreport(DIGITAL_LABEL_SUBREPORT));
-		} else if (sender == printScheduleButton) {
+		} else if (scheduleButton.equals(sender)) {
 			print(new ReportDto().setName(SCHEDULE));
-		} else if (sender == printTestButton) {
+		} else if (testButton.equals(sender)) {
 			//Create a ListBox
 			ListBox testListBox = new ListBox();
 			testListBox.addItem("Testing");
@@ -126,6 +144,6 @@ public class Dashboard implements EntryPoint, ClickHandler, ReportPrinter {
 	}
 	
 	public String getSelectedPrinter() {
-		return printerListBox.getItemText(printerListBox.getSelectedIndex());
+		return printerListBox.getValue(printerListBox.getSelectedIndex());
 	}
 }
